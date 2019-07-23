a33a518bf3a816a68fc9e0110089c98dd16ad9a4
Ayomikun Oloyede <o.ayomikun83@gmail.com>
2019-07-23 06:58:00 +0100

    Second git change

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ router.get('/register', (req, res) =>{
 //login Form Post
 router.post('/login', (req, res, next) =>{
   passport.authenticate('local', {
-    successRedirect: '/ideas',
+    successRedirect: '/',
     failureRedirect: '/users/login',
     failureFlash: true
   })(req, res, next);
